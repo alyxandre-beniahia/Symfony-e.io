cb769fa8912a11aec2321ad0e01d1b5e081ea2e7
Beniahia <alyxandre.beniahia@laplateforme.io>
2025-07-08 10:30:58 +0200

    ajout des pages d'inscription/connexion

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -4,10 +4,23 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AuthController extends AbstractController
 {
+    #[Route('/login', name: 'app_login', methods: ['GET'])]
+    public function loginPage(): Response
+    {
+        return $this->render('auth/login.html.twig');
+    }
+
+    #[Route('/register', name: 'app_register', methods: ['GET'])]
+    public function registerPage(): Response
+    {
+        return $this->render('auth/register.html.twig');
+    }
+
     #[Route('/api/login_check', name: 'api_login_check', methods: ['POST'])]
     public function login(): JsonResponse
     {
